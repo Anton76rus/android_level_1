a09bd80e5d55cfc5b8a43899db40248973d7eb00
Anton76rus <yazgevich@gmail.com>
2019-08-28 11:43:41 +0300

    Добавлен метод проверки на победу и ничью. Добавлен метод хода компьютера. Завершён игровой цикл.

--- Homework3_TicTacToe/src/TicTacToe.java
+++ Homework3_TicTacToe/src/TicTacToe.java
@@ -12,6 +12,7 @@ public class TicTacToe {
     private static Scanner SCANNER = new Scanner(System.in);
     private static Random RANDOM = new Random();
 
+    private static final int WIN = 3;
     private static final int fieldSizeX = 3;
     private static final int fieldSizeY = 3;
     private static char[][] field = new char[fieldSizeY][fieldSizeX];
@@ -22,6 +23,14 @@ public class TicTacToe {
 
     private static boolean isEmptyDot(int x,int y){
         return field[x][y] == DOT_EMPTY;
+    }
+
+    private static boolean isHumanDot(int x,int y){
+        return field[x][y] == DOT_X;
+    }
+
+    private static boolean isAIDot(int x,int y){
+        return field[x][y] == DOT_O;
     }
 
     private static void initMap(){
@@ -43,7 +52,7 @@ public class TicTacToe {
         }
     }
 
-    private static void humanTurn (){
+    private static void humanTurn(){
         int x;
         int y;
         System.out.println("\nВаш ход!!!");
@@ -51,14 +60,63 @@ public class TicTacToe {
             System.out.println("Введите координаты хода Х и Y (от 1 до " + field.length + ") через пробел");
             y = SCANNER.nextInt() - 1;
             x = SCANNER.nextInt() - 1;
-        }while (!isValidCell(x,y) && isValidCell(x,y));
+        }while (!isValidCell(x,y) && !isEmptyDot(y,x) && isAIDot(y,x));
         field[x][y] = DOT_X;
+    }
+
+    private static void aITurn(){
+        int x;
+        int y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        }while (!isEmptyDot(x,y) && isHumanDot(x,y));
+        field[x][y] = DOT_O;
+    }
+
+    private static boolean checkWin(int dot){
+        int count = 0;
+        for (int x = 0;x < fieldSizeX;x++){
+            for (int y = 0;y < fieldSizeY;y++){
+                if (field[x][y] == dot) count++;
+            }
+        }
+        return count >= WIN ? true : false;
+    }
+
+    private static boolean checkDraw(){
+        for (int y = 0;y < fieldSizeY;y++){
+            for (int x = 0; x < fieldSizeX;x++){
+                if(field[y][x] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args){
         initMap();
         printMap();
-        humanTurn();
-        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Winner winner chicken dinner!!!");
+                break;
+            }
+            if (checkDraw()){
+                System.out.println("Опачки!Ничья!");
+                break;
+            }
+            aITurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил кампуктер !!! Лол");
+                break;
+            }
+            if (checkDraw()){
+                System.out.println("Опачки!Ничья!");
+                break;
+            }
+        }
     }
 }
